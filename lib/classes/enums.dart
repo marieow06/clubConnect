17065e029a8101e1810a98f0a0026e12aa7a3350
Maria <mariferreyesmota938@gmail.com>
2026-01-05 21:45:52 -0600

    Add Events class to enums.dart

--- lib/classes/enums.dart
+++ lib/classes/enums.dart
@@ -1,4 +1,13 @@
-enum AppRole {
-  admin,
-  student
+class Events {
+  final String id;
+  final String title;
+  final String description;
+  final String status;
+
+  Events({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.status = 'active',
+});
 }
