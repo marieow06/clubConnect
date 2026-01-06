634f4a4917b226dce29b3c3563e2dec135429e9a
Maria <mariferreyesmota938@gmail.com>
2026-01-05 21:46:10 -0600

    Update enums.dart

--- lib/classes/enums.dart
+++ lib/classes/enums.dart
@@ -1,13 +1,4 @@
-class Events {
-  final String id;
-  final String title;
-  final String description;
-  final String status;
-
-  Events({
-    required this.id,
-    required this.title,
-    required this.description,
-    this.status = 'active',
-});
+enum AppRole {
+  admin,
+  student
 }
