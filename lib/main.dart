c87a6aed15da50bb0d492c152c8376684ab00a7b
Maria <mariferreyesmota938@gmail.com>
2026-01-05 21:30:02 -0600

    Refactor main function and update app routing
    Refactored main function to initialize Firebase and handle notifications. Updated routing for student and admin roles.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,47 @@
-import 'pages/studentlogin.dart';
+import 'package:club_connect/Calendars/adminCalendar.dart';
+import 'package:club_connect/admins/adminHome.dart';
+import 'package:club_connect/students/Register.dart';
+import 'package:club_connect/admins/admin.dart';
+import 'package:club_connect/Calendars/studentcalendar.dart';
+import 'package:club_connect/students/forgotPassword.dart';
+import 'package:club_connect/students/home.dart';
+import 'package:club_connect/students/student.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'classes/enums.dart';
-import 'pages/home.dart';
-import 'pages/clubs.dart';
-import 'pages/admin.dart';
-import 'pages/Register.dart';
-import 'pages/forgotPassword.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => Login(role: AppRole.student),
-      '/forgot_password': (context) => forgotPassword(),
-      '/admin': (context) => Admin(role: AppRole.admin),
-      '/createAcc': (context) => Register(role: AppRole.student),
-      '/home': (context) => Home(),
-      '/clubs': (context) => Clubs(),
-    },
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Make sure Flutter is initialized
+  FirebaseMessaging.onMessage.listen((message) {
+    print('Notification received: ${message.notification?.title}');
+  });
+
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        //student
+        '/': (context) => Login(role: AppRole.student),
+        '/forgot_password': (context) => forgotPassword(),
+        '/register': (context) => Register(role: AppRole.student),
+        '/home': (context) => Home(role: AppRole.student),
+        '/studentcalendar': (context) => Calendar(),
+
+        // admin
+        '/admin': (context) => Admin(role: AppRole.admin),
+        '/adminHome': (context) => adminhome(),
+        '/adminCalendar': (context) => admincalendar(),
+      },
+    );
+  }
 }
