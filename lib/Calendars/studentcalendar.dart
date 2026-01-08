05eb0fdfdf30ac2dd1d3d64538ef30babe138897
Maria <mariferreyesmota938@gmail.com>
2026-01-07 22:19:47 -0600

    Update studentcalendar.dart

--- lib/Calendars/studentcalendar.dart
+++ lib/Calendars/studentcalendar.dart
@@ -120,11 +120,136 @@ class _CalendarState extends State<Calendar> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
-          '~~ Club Calendar ~~',
+          'Club Calendar',
           style: TextStyle(fontSize: 25),
         ),
         centerTitle: true,
         backgroundColor: const Color(0xFFFFDC79),
+        leading: Builder(
+          builder: (context) => IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer(); // Opens the menu
+            },
+            icon: Icon(Icons.menu),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Color(0xFFFFDC79),
+        // holds the content of the menu
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(height: 65.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              style: TextButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 25.0, // same height as text for visual alignment
+                  ),
+                  SizedBox(width: 10), // spacing between icon and text
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 1.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/studentcalendar');
+              },
+              style: TextButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.event,
+                    color: Colors.black,
+                    size: 25.0,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Calendar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 1.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/attendance');
+              },
+              style: TextButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.how_to_reg,
+                    color: Colors.black,
+                    size: 25.0,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Attendance',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 1.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/clubs');
+              },
+              style: TextButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.groups,
+                    color: Colors.black,
+                    size: 25.0,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Clubs',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       body: Column(
         children: [
