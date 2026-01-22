f3d4791f1c4078ba37bc78fffb783d55c45803df
Maria <mariferreyesmota938@gmail.com>
2026-01-22 07:27:46 -0600

    Update adminHome.dart

--- lib/admins/adminHome.dart
+++ lib/admins/adminHome.dart
@@ -9,40 +9,112 @@ class adminhome extends StatefulWidget {
 }
 
 class _adminhomeState extends State<adminhome> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color(0xFFFFDC79),
         elevation: 0,
-      ),
-      extendBodyBehindAppBar: true,
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/pictures/Catbckgrnd.webp',
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          SizedBox(height: 9.0),
-          TextButton(
+        leading: Builder(
+          builder: (context) => IconButton(
             onPressed: () {
-              Navigator.pushNamed(context, '/adminCalendar');
+              Scaffold.of(context).openDrawer(); // Opens the menu
             },
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                'Calendar?',
-                style: TextStyle(
-                  color: Color(0xFF7A4C10),
-                  fontSize: 21.0,
-                  decoration: TextDecoration.underline,
-                ),
+            icon: Icon(Icons.menu),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Color(0xFFFFDC79),
+        // holds the content of the menu
+        child: ListView(padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(height: 65.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+                },
+              style: TextButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.home,
+                    color: Colors.black,
+                    size: 25.0, // same height as text for visual alignment
+                  ),
+                  SizedBox(width: 10), // spacing between icon and text
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 1.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/studentcalendar');
+                },
+              style: TextButton.styleFrom(
+                    alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.event,
+                    color: Colors.black,
+                    size: 25.0,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Calendar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 1.0),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/attendance');
+                },
+              style: TextButton.styleFrom(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.how_to_reg,
+                    color: Colors.black,
+                    size: 25.0,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Attendance',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
